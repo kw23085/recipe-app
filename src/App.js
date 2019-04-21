dd859c8cacdc2b2f27c2e93ffbe3d26471b6b40e
Kevin Wang <kw1017@gmail.com>
2019-04-21 16:04:53 -0700

    added api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Form from "./components/Form";
+
+const API_KEY = "9d04ac8c044a10f960329cb97a0bcd97";
+
+
 class App extends Component {
+  getRecipe = async (e) => {
+    const recipeNmae = e.target.elements.recipeName.value
+    e.preventDefault();
+    const api_call = await fetch(`http://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&page=2`);
+    
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }

7ac1ce17c7b0aea9f17365c7a7e1f5ffb0ec6c08
Kevin Wang <kw1017@gmail.com>
2019-04-22 20:57:51 -0700

    list out all the recipe name on screen

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ const API_KEY = "9d04ac8c044a10f960329cb97a0bcd97";
 
 
 class App extends Component {
+
+  state = {
+    recipes: []  
+  }
+  
   getRecipe = async (e) => {
     const recipeNmae = e.target.elements.recipeName.value
     e.preventDefault();
     const api_call = await fetch(`http://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&page=2`);
     
     const data = await api_call.json();
-    console.log(data);
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes)
   }
   render() {
     return (
@@ -22,6 +28,9 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        { this.state.recipes.map((recipe) => {
+          return <p key={recipe.recipe_id}>{ recipe.title }</p>
+        }) }
       </div>
     );
   }

4239b13355dde9e17c39b2d79399c3a25b642a60
Kevin Wang <kw1017@gmail.com>
2019-04-25 23:57:40 -0700

    add css

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,8 +1,23 @@
 import React from "react";
 
-const Recipes = () => (
-    <div>
-        Recipes!
+const Recipes = props => (
+    <div className="container">
+        <div className="row">
+            { props.recipes.map((recipe) => {
+            return (
+                <div className="col-md-4">
+                    <div className="recipe__box">
+                        <img 
+                            className="recipe__box-img" 
+                            src={rec.img_url} 
+                            alt={recipe.title}/>
+                            <div className="recipe__text">
+                            </div>    
+                    </div>   
+                </div>
+                );
+            }) }
+        </div>
     </div>
 );
 

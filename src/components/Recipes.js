a416eaf998703f6a5a645190cf574793fe176a59
Kevin Wang <kw1017@gmail.com>
2019-04-27 18:56:33 -0700

    added style button for result

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -5,14 +5,19 @@ const Recipes = props => (
         <div className="row">
             { props.recipes.map((recipe) => {
             return (
-                <div className="col-md-4">
+                <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }}>
                     <div className="recipe__box">
                         <img 
                             className="recipe__box-img" 
-                            src={rec.img_url} 
+                            src={recipe.image_url} 
                             alt={recipe.title}/>
                             <div className="recipe__text">
+                                <h5 className="recipes__title">{ recipe.title }</h5>
+                                <p className="recipes__subtitle">Publisher
+                                    <span>{ recipe.publisher }</span>
+                                </p>
                             </div>    
+                            <button className="recipe__button">View Recipe</button>
                     </div>   
                 </div>
                 );

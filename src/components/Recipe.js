dcb58cc2081d702f83d98c9eedc428790371ef18
Kevin Wang <kw1017@gmail.com>
2019-04-30 23:37:13 -0700

    render publisher website url onto recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -13,13 +13,29 @@ class Recipe extends React.Component {
         const req = await fetch(`http://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
         
         const res = await req.json();
-        console.log(res);
+        this.setState({ activeRecipe: res.recipes[0] });
+        console.log(this.state.activeRecipe);
     }
 
     render() {
-        console.log(this.props);
+        const recipe = this.state.activeRecipe;
         return (
-          <div>Recipe Component!</div>  
+          <div className="container">
+            <div className="active-recipe">
+                <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title}/>
+                <h3 className="active-recipe__title">
+                    {recipe.title}
+                </h3>
+                <h4 className="active-recipe__publisher">
+                    Publisher: <span>{ recipe.publisher }</span>
+                </h4>
+                <p className="active-recipe__website">
+                    Website:
+                    <span><a href="{recipe.publisher_url}">{recipe.publisher_url}</a></span>
+                </p>
+                <button className="active-recipe__button">Go Home</button>
+            </div>
+          </div>
         );
     }
 }
